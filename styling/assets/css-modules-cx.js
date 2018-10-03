bcda75da25f0eed52f19552faa4c12709edce9d5
Christopher Baker <christopher@bitovi.com>
2018-10-03 10:37:41 -0500

    finish styling

--- styling/assets/css-modules-cx.js
+++ styling/assets/css-modules-cx.js
@@ -2,10 +2,14 @@ import styles from './css-modules.css';
 import cx from 'classnames';
 
 function Thing() {
-  const { bar } = this.props;
+  const { bar, active } = this.props;
 
   return (
-    <div className={cx(styles.foo, bar && styles.bar)}>
+    <div className={cx(
+      styles.foo,
+      bar && styles.bar,
+      { 'active': active },
+    )}>
       Hello World!
     </div>
   );
