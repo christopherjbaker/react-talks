4c8b308ff80c11d0c08550123d03f1c725fbd871
Christopher Baker <christopher@bitovi.com>
2019-05-06 21:14:03 -0700

    Updates

--- intro/assets/inline-functions-bad.js
+++
@@ -1,9 +0,0 @@
-function Counter({ value, onChange }) {
-  return (
-    <div>
-      <button onClick={() => onChange(value - 1)}>--</button>
-      <span>{value}</span>
-      <button onClick={() => onChange(value + 1)}>++</button>
-    </div>
-  );
-}
